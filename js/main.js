c6098aea5cfb754d26b7032e9b68e03c1e5fbbba
Victoria045 <victoriaberyl45@gmail.com>
2021-09-20 15:41:56 +0300

    modify shopping cart

--- js/main.js
+++ js/main.js
@@ -103,32 +103,10 @@ function modifiedUI() {
 
   let subTotalPrice = 0;
 
-  $('#shoppingList').html('');
-  for(let i=0; i<cart.cartItems.length; i++){
-      const item = cart.cartItems[i];
-      const crustPrice = item.crust ? item.crust.price : 0;
-      let toppingPrice = 0;
-      if(item.topping.length > 0){
-          toppingPrice = item.topping.reduce((a, b) => a+b.price, 0);
-      }
-     
-      subTotalPrice += item.price + crustPrice + toppingPrice;
-
-      $('#shoppingList').append(cartItemElement);        
-      $('#shoppingList span#type').html(item.name);
-      $('#shoppingList span#size').html(item.size);
-      if(item.crust) 
-          $('#shoppingList span#slCrust')
-          .append("Crust:"+item.crust.name)
-
-      if(item.topping) $('#shoppingList span#slTopping')
-          .append(" Topping:"+item.topping.map(topping => topping.name).join(','));   
-  }
-
   $("#totalPrice").html(subTotalPrice);
-  // $("#totalPrice").html(
-  //   subTotalPrice + (checkout.delivery ? checkout.delivery.price : 0)
-  // );
+  $("#totalPrice").html(
+    subTotalPrice + (checkout.delivery ? checkout.delivery.price : 0)
+  );
 }
 
 $(document).ready(function () {
